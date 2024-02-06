3292cb33b33f50da5c2f86248ad2e3e5bc51ed59
apocsenpai <jonatassennas@ufrrj.br>
2024-02-06 16:48:48 -0300

    feat: implements user repository

---
+++ api/topmed-test/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using topmed_test.Models.DTOs;
+using topmed_test.Models.Entities;
+
+namespace topmed_test.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> FindByCredentials(AuthDTO credentials);
+    }
+}
---
+++ api/topmed-test/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using topmed_test.Data;
+using topmed_test.Interfaces;
+using topmed_test.Models.DTOs;
+using topmed_test.Models.Entities;
+
+namespace topmed_test.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public UserRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<User> FindByCredentials(AuthDTO credentials) =>
+            await _dataContext.Users.Where(u => u.Email == credentials.Email && u.Password == credentials.Password).FirstOrDefaultAsync();
+    }
+}
+
