8a11e4e2e8e3c783d291304c9c244250eb679ffd
apocsenpai <jonatassennas@ufrrj.br>
2024-02-06 16:51:09 -0300

    tests: implement unit tests

---
+++ api/topmed_test.Tests/Models/Entities/UserTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Models.Entities;
+
+
+namespace API.Tests.Models.Entities
+{
+    public class UserTests
+    {
+        [Fact]
+        public void User_Validate_Email_Length()
+        {
+            var result = Assert.Throws<DomainException>(() => new User())
+            }
+    }
+}
