bb1e2116cc7e51a88f83e54b00c9f3874f55436d
apocsenpai <jonatassennas@ufrrj.br>
2024-02-06 16:48:21 -0300

    feat: implements tokenservice

---
+++ api/topmed-test/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using topmed_test.Models.Entities;
+
+namespace topmed_test.Interfaces
+{
+    public interface ITokenService
+    {
+        string GetToken(User _userData);
+    }
+}
---
+++ api/topmed-test/Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using topmed_test.Interfaces;
+using topmed_test.Models.Entities;
+
+namespace topmed_test.Services
+{
+    public class TokenService : ITokenService
+    {
+        IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetToken(User _userData)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("Email", _userData.Email),
+                    };
+
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+
+            var signIn = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+            var accessToken = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddHours(10),
+                signingCredentials: signIn);
+
+
+            return tokenHandler.WriteToken(accessToken);
+        }
+    }
+}
